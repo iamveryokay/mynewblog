e93cd05f3e2be08a1240d072a723f51f1ae9965b
iamveryokay <zjw_@alumni.sjtu.edu.cn>
2020-12-23 00:16:01 +0800

    v4

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,4 +33,5 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME = 'notmyidea'
+#THEME = 'notmyidea'
+THEME = 'simple'
